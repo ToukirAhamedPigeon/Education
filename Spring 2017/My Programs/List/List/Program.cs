c32fa02a8a1f835998af3b614427e29de227120a
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2017-02-13 20:56:21 +0600

    My Programs

---
+++ Spring 2017/My Programs/List/List/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        string[] list=new string[100];
+        static void command(string cmd)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            string cmd;
+            Console.WriteLine("Write 'PUSH, item' to push item");
+            Console.WriteLine("Write 'PUSH, item, after, listed item' to push item");
+            Console.WriteLine("Write 'PUSH, item, before, listed item' to push item");
+            Console.WriteLine("Write 'DELETE, item' to delete item");
+            Console.WriteLine("Write 'CLOSE' to close program");
+            for (;;)
+            {
+                Console.Write("Command: ");
+                cmd = Console.ReadLine();
+                command(cmd);
+            }
+        }
+    }
+}
